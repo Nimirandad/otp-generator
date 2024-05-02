c7abf75c57e31ee114c9343fad156a65f345effa
nicolas.miranda <nicolas.miranda@tocbiometrics.com>
2024-05-02 18:29:11 -0400

    otp generator commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Nimirandad/otp-generator
+
+go 1.22.0
---
+++ otp.go
@@ -0,0 +1,57 @@
+package otp
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+type Chars struct {
+	Digits             string
+	LowerCaseAlphabets string
+	UpperCaseAlphabets string
+	SpecialChars       string
+}
+
+func OTP() *Chars {
+	return &Chars{
+		Digits:             "0123456789",
+		LowerCaseAlphabets: "abcdefghijklmnopqrstuvwxyz",
+		UpperCaseAlphabets: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		SpecialChars:       "#!&@",
+	}
+}
+
+func (c *Chars) Generate(length int, digits bool, lowerCase bool, upperCase bool, specialChars bool) string {
+	allowedChars := ""
+	password := ""
+
+	if digits {
+		allowedChars += c.Digits
+	}
+
+	if lowerCase {
+		allowedChars += c.LowerCaseAlphabets
+	}
+
+	if upperCase {
+		allowedChars += c.UpperCaseAlphabets
+	}
+
+	if specialChars {
+		allowedChars += c.SpecialChars
+	}
+
+	usedChars := make(map[rune]bool)
+
+	for len(password) < length {
+		charIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(allowedChars))))
+		randomChar := rune(allowedChars[charIndex.Int64()])
+
+		if !usedChars[randomChar] {
+			password += string(randomChar)
+			usedChars[randomChar] = true
+		}
+	}
+
+	return password
+}
